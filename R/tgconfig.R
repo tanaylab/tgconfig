e68351c3ccf871bb9df244617ad21076d3a80a97
Aviezer Lifshitz <18175550+aviezerl@users.noreply.github.com>
2017-07-30 19:40:25 +0300

    added examples

--- R/tgconfig.R
+++ R/tgconfig.R
@@ -18,8 +18,8 @@ get_config <- function(param, package){
 #'
 #' @return name of the package the code is invoked from, and NULL if it is not run from a package
 #'
-#' @example
-#' guess_pacakge(parent.frame(n=2))
+#' @examples
+#' guess_package(environment())
 guess_package <- function(env){
 	package <- utils::packageName(env=parent.frame(n=2))
 	if (is.null(package)){
@@ -38,10 +38,17 @@ guess_package <- function(env){
 #'
 #' @return value of \code{param} in package \code{package} and \code{fallback} if parameter not found
 #'
-#' @example
+#' @examples
+#'
 #' register_param('param1', 'tgconfig')
 #' set_param('param1', 'value1', 'tgconfig')
 #' get_param('param1', 'tgconfig')
+#'
+#' # try to get a parameter that doesn't exist
+#' get_param('other_param', 'tgconfig')
+#'
+#' # sometimes we want to throw and error if the parameter doesn't exist
+#' get_param('other_param', 'tgconfig', fallback=stop())
 #'
 #'
 #' @export
@@ -61,12 +68,15 @@ get_param <- function(param, package=NULL, fallback=NULL){
 #' @param value value to set the parameter to
 #' @param package package of the parameter (NULL if running from a package)
 #'
-#' @example
+#' @examples
 #' register_param('param1', 'tgconfig')
 #' set_param('param1', 'value1', 'tgconfig')
 #' get_param('param1', 'tgconfig')
-#' 
-#' \@seealso register_param, get_param
+#'
+#' # try to set a parameter that doesn't exist
+#' set_param('other_param', 'tgconfig')
+#'
+#' @seealso register_param, get_param
 #'
 #'
 #' @export
@@ -86,7 +96,7 @@ set_param <- function(param, value, package=NULL){
 #'
 #' @return a list with package parameters and values. NULL if \code{package} has no parameters
 #'
-#' @example
+#' @examples
 #' register_param('param1', 'tgconfig')
 #' register_param('param2', 'tgconfig')
 #' set_param('param1', 'value1', 'tgconfig')
@@ -103,7 +113,7 @@ get_package_params <- function(package){
 #'
 #' @return names of package parameters. NULL if \code{package} has no parameters
 #'
-#' @example
+#' @examples
 #' register_param('param1', 'tgconfig')
 #' register_param('param2', 'tgconfig')
 #' set_param('param1', 'value1', 'tgconfig')
@@ -120,7 +130,7 @@ list_package_params <- function(package){
 #'
 #' @param package package
 #'
-#' @example
+#' @examples
 #' register_param('param1', 'tgconfig')
 #' has_param('param1', 'tgconfig')
 #' has_param('param2', 'tgconfig')
@@ -138,7 +148,7 @@ has_param <- function(param, package=NULL){
 #' @param package package to register parameter to
 #' @param default_value default value of the parameter (default: NA)
 #'
-#' @example
+#' @examples
 #' register_param('param1', 'tgconfig')
 #' get_package_parame('tgconfig')
 #'
@@ -156,15 +166,15 @@ register_param <- function(param, package=NULL, default_value=NA){
 #' @param config_file yaml file with paramters and values
 #'
 #' @param package package
-#' 
-#' @example
+#'
+#' @examples
 #' config_file <- example_config_file()
 #' register_params(config_file, 'tgconfig')
 #' get_package_params('tgconfig')
 #' override_params(system.file('config/override_example.yaml', package='tgconfig'))
 #' get_package_params('tgconfig')
-#' 
-#' 
+#'
+#'
 #' @export
 override_params <- function(config_file, package=NULL){
 	package <- package %||% guess_package(parent.frame(n=2))
@@ -184,8 +194,8 @@ override_params <- function(config_file, package=NULL){
 #' @param config_file yaml file with paramters and values
 #'
 #' @param package package
-#' 
-#' @example
+#'
+#' @examples
 #' config_file <- example_config_file()
 #' register_params(config_file, 'tgconfig')
 #' get_package_params('tgconfig')
@@ -204,7 +214,7 @@ register_params <- function(config_file, package=NULL){
 }
 
 #' Get example config file path
-#' 
+#'
 #' @export
 example_config_file <- function(){
 	system.file('config/example.yaml', package='tgconfig')
@@ -213,9 +223,9 @@ example_config_file <- function(){
 
 
 #' Dump example config file
-#' 
+#'
 #' @param path path to dump example config file to
-#'  
+#'
 #' @export
 dump_example_config <- function(path){
 	file.copy(example_config_file(), path)
